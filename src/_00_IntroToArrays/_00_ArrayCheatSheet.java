19b0d082b75462bcdfe05fa60e9845494f5afbcf
Jgooso <joshgoodman0220@gmail.com>
2020-07-18 12:14:33 -0700

    asdfghjk

--- src/_00_IntroToArrays/_00_ArrayCheatSheet.java
+++ src/_00_IntroToArrays/_00_ArrayCheatSheet.java
@@ -5,29 +5,52 @@ import java.util.Random;
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-
-		//2. print the third element in the array
-
-		//3. set the third element to a different value
-
-		//4. print the third element again
+		String[] str = {"a","b","c","d","e"};
 		
+		//2. print the third element in the array
+		System.out.println(str[3]);
+		//3. set the third element to a different value
+		str[3] = "b";
+		//4. print the third element again
+		System.out.println(str[3]);
 		//5. use a for loop to set all the elements in the array to a string of your choice
+		for(int i = 0; i< str.length;i++) {
+			str[i] = "a";
+		}
 		
 		//6. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		for(int i = 0; i< str.length;i++) {
+			System.out.print(str[i]);
+		}
 		//7. make an array of 50 integers
-
+		int[] it = new int[50];
 		//8. use a for loop to make every value of the integer array a random number
-
+		for(int i = 0; i < it.length;i++) {
+			it[i] = new Random().nextInt(50);
+		}
 		//9. without printing the entire array, print only the smallest number on the array
-
+		int lowest = 999;
+		for(int i = 0; i < it.length;i++) {
+			if(it[i] < lowest) {
+				lowest = it[i];
+			}
+		}
+		System.out.println(lowest);
 		//10 print the entire array to see if step 8 was correct
-
+		for(int i = 0; i < it.length;i++) {
+			System.out.print(it[i] + ", ");
+		}
 		//11. print the largest number in the array.
-		
+		int highest = 0;
+		for(int x = 0; x < it.length;x++) {
+			if(it[x] > highest) {
+				highest = it[x];
+			}
+		}
+		System.out.println();
+		System.out.println(highest + "h");
 		//12. print only the last element in the array
-		
+		System.out.println(it.length-1);
 	}
 }
--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -4,23 +4,67 @@ import java.util.Random;
 
 public class MoreArrayFun {
 	//1. Create a main method to test the other methods you write.
+	public static void main(String[] args) {
+		String[] strings = {"a","b","c","d","e"};
+		printStrings(strings);
+		printStringsReverse(strings);
+		printStringsOther(strings);
+		printRandomString(strings);
+	}
 	
 	
-	
-	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
-	
+	static //2. Write a method that takes an array of Strings and prints all the Strings in the array.
+	void printStrings(String[] arr) {
+		for(int i = 0; i < arr.length;i++) {
+			System.out.print(arr[i]);
+		}
+		System.out.println();
+	}
 	
 	
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
-	
+	static void printStringsReverse(String[] arr) {
+		for(int i = arr.length-1; i >= 0;i--) {
+			System.out.print(arr[i]);
+		}
+		System.out.println();
+		
+	}
 	
 	
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
-	
+	static void printStringsOther(String[] arr) {
+		for(int i = 0; i < arr.length;i+=2) {
+			System.out.print(arr[i]);
+		}
+		System.out.println();
+	}
 	
 	//5. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in a completely random order. Almost every run of the program should result in a different order.
-	
+	static void printRandomString(String[] arr) {
+		boolean[] selected = new boolean[arr.length];
+		boolean contains = false;
+		Random ran = new Random();
+		int index;
+		for(int i = 0;i < selected.length;i++) {
+			selected[i] = true;
+		}
+		while(contains == false) {
+			contains = true;
+			index = ran.nextInt(arr.length);
+			
+			if(selected[index] == true) {
+			System.out.print(arr[index]);
+			}
+			selected[index] = false;
+			for(int i = 0;i < selected.length;i++) {
+				if(selected[i] == true) {
+					contains = false;
+				}
+			}
+		}
+	}
 	
 }
